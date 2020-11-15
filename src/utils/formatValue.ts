ec4011d47aee46bbd4ea33d3ab9a8289e4f7b5ab
Túlio Gaio <tuliogaio@gmail.com>
2020-11-15 09:44:05 -0300

    GoStack - Desafios - Fundamentos ReactJs

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,8 @@
 const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+  Intl.NumberFormat('pt-br', {
+    style: 'currency',
+    currency: 'BRL',
+    maximumSignificantDigits: 2,
+  }).format(value);
 
 export default formatValue;
